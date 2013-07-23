0d55cc4edd84cfbc5cf8d675669c8eb5d3441a21
Mike Phillips <phill2mj@gmail.com>
2013-07-23 09:32:54 -0400

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cwc::Application.config.secret_token = '709e5e074d0eafcd43c2f075081be65504cce1bf5fbe1dd119414d4c68cbd545eede843f37f6025818b647651cbc173839cddc033594b8bf6f9e080a5cdb35ef'
